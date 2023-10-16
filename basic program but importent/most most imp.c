b83ad3b80e1ee28d06c241025b9a8061a88ca981
soul059 <136678396+soul059@users.noreply.github.com>
2023-10-16 22:25:38 +0530

    Update most most imp.c

--- basic program but importent/most most imp.c
+++ basic program but importent/most most imp.c
@@ -4,9 +4,13 @@ int main()
 {
 
     int a,b;
-    int i =scanf("%d %d",&a,&b);
+    int i =scanf("%d %d",&a,&b);// 3 , 4
     int j = printf("hello\n");
 
     printf("%d,%d",i,j);
     return 0;
 }
+
+// output will be
+// hello
+// 2,6
