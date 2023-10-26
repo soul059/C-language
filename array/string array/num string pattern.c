46e3836ca4363bb29a54294fbaa7bf5c1ae8b91d
soul059 <136678396+soul059@users.noreply.github.com>
2023-10-26 08:37:44 +0530

    Add files via upload

--- array/string array/num string pattern.c
+++ array/string array/num string pattern.c
@@ -6,20 +6,20 @@ int main()
     char str[30]="123456789";
     int i,j;
     int l = strlen(str);
-    j=l-1;
+    j=l;
     i=0;
 
     while(i<l/2+1)
     {
         printf("%*.*s",5,i+1,&str[i]);
         
-        strrev(str);
+        strrev(str);// 987654321
         
         printf("%*.*s",-5,i,&str[j]);
         printf("\n");
 
         strrev(str);
-        j--;
+        j-=2;
         i++;
     }
     return 0;
